df9efd1eb3652d6f3ad40ce0f5651cf9958a53ff
josemanriquejulialopez <josepthebest@gmail.com>
2017-05-29 17:06:27 +0200

    #3 bily i  trust in you

--- app/src/main/java/com/example/billy/jumpit/controller/activities/Scenes/HistoryScene.java
+++ app/src/main/java/com/example/billy/jumpit/controller/activities/Scenes/HistoryScene.java
@@ -24,9 +24,13 @@ import java.util.List;
 
 public class HistoryScene {
     public final static int SCENE_HEIGHT = 16;
+    private char scene2[][] = new char[16][30];
     private String[] scene;
     private Paint paint;
+    private int cont = 0;
     private GameViewHistoria game;
+    boolean creatingPlatform = false;
+    private int platformsDistance = 0;
     private BitmapSet bitmapSet;
     public HistoryScene(GameViewHistoria game) {
         this.game = game;
@@ -81,11 +85,15 @@ public class HistoryScene {
         return i;
     }
     public void draw(Canvas canvas) {
+        if (cont > 15) {
+            scene2 = updateMap();
+            cont = 0;
+        }
         if (scene == null) return;
         for(int y = 0; y < scene.length; y++) {
             for(int x = 0; x < scene[0].length(); x++) {
                 Bitmap bitmap;
-                Log.e("draw",""+scene[y].charAt(x));
+//                Log.e("draw",""+scene[y].charAt(x));
                 switch (scene[y].charAt(x)) {
                     case '.':
                         bitmap = bitmapSet.getBitmap(23);
@@ -97,8 +105,83 @@ public class HistoryScene {
                         bitmap = bitmapSet.getBitmap(23);
                         break;
                 }
-                canvas.drawBitmap(bitmap, x * 16 , y * 16, paint);
+                canvas.drawBitmap(bitmap, x * 16 - cont, y * 16, paint);
             }
         }
+        cont = cont + 4;
+    }
+
+    public char[][] updateMap() {
+        int platforms = 0;
+        int platformsLong1 = 0;
+        int platformsLong2 = 0;
+        int platformsLong3 = 0;
+        int groundCounter = 0;
+        int rng = 0;
+        int creatingPlatformLong = 0;
+        int lastPlatformAltitude = 15;
+        int checkRng = 0;
+        int lastPlatformX = 20;
+        platformsDistance++;
+        // buscamos info
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 29; j++) {
+                // contamos las plataformas
+                if (scene2[i][j] == ('-'/* plataforma */) && scene2[i][j + 1] == ('.' /* cielo */)) {
+                    platforms++;
+                    lastPlatformX = j;
+                }
+                // contamos el suelo
+                if (i == 15 && scene2[15][j] == '-') {
+                    groundCounter++;
+                }
+                if ( scene2[i][29] == ('-'/* plataforma */)){
+                    lastPlatformAltitude = i;
+                }
+            }
+        }
+        // avanzamos el scene una posicion
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 29; j++) {
+                scene2[i][j] = scene2[i][j + 1];
+            }
+        }
+        // pintamos la ultima linea
+        for (int i = 0; i < 16; i++) {
+//            if (i == 15 && groundCounter < 3) {
+//                scene[i][29] = '-';
+//                creatingPlatform = true;
+//            }else {
+            //pintar plataforma nueva
+            if (platforms < 6 && i == 15 && lastPlatformX<25 && !creatingPlatform) {
+                do {
+                    rng = (int) (Math.random() * 15 );
+                    checkRng = rng-lastPlatformAltitude;
+                }while(rng<6 || checkRng>2 || checkRng<(-2) );
+                scene2[rng][29] = '-';
+                platformsDistance = 0;
+                creatingPlatform = true;
+            }else {
+                // pintar cielo
+                scene2[i][29] = '.';
+            }
+            // random de la longitud de la plataforma
+            if (scene2[i][28] == '-') {
+                for (int j = 28; j > 24; j--) {
+                    if (scene2[i][j] == '-')
+                        creatingPlatformLong++;
+                }
+                rng = (int) (Math.random() * 10 + creatingPlatformLong);
+
+                if (rng < 11)
+                    scene2[i][29] = '-';
+                else
+                    creatingPlatform = false;
+            }
+
+        }
+//        }
+        return scene2;
+
     }
 }
--- app/src/main/java/com/example/billy/jumpit/controller/activities/main/MainActivity.java
+++ app/src/main/java/com/example/billy/jumpit/controller/activities/main/MainActivity.java
@@ -223,7 +223,8 @@ public void onClick(View v) {
         volumeControl = (SeekBar)findViewById(R.id.volumeBar);
         vistaTienda = (View)findViewById(R.id.shopView);
         menuniveles = (View)findViewById(R.id.vistaNiveles);
-        final GameView gameView = (GameView) findViewById(R.id.view4);
+        //final GameView gameView = (GameView) findViewById(R.id.view4);
+        final GameViewHistoria gameView = (GameViewHistoria) findViewById(R.id.viewGameHistoria);
         final View mainMenuView = (View)findViewById(R.id.view);
         final ImageButton pause = (ImageButton)findViewById(R.id.pause);
         reloadEndless = (ImageButton)findViewById(R.id.reloadEndless);
