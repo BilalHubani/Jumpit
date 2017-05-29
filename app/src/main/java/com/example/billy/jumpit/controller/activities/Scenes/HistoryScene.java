da06fd06afd18d0bb7c09ede26cfb7ed57163e47
BilalHubani <bilal.hubani@gmail.com>
2017-05-29 17:38:05 +0200

    #3 HistoryScene started

--- app/src/main/java/com/example/billy/jumpit/controller/activities/Scenes/HistoryScene.java
+++ app/src/main/java/com/example/billy/jumpit/controller/activities/Scenes/HistoryScene.java
@@ -69,7 +69,6 @@ public class HistoryScene {
 
     public int getBitmap(int r, int c) {
         char e = scene[r].charAt(c);
-        Log.e("getBitmap", ""+e);
         int i = -1;
         switch (e) {
             case '<': i = 35; break;
@@ -101,6 +100,9 @@ public class HistoryScene {
                     case '-':
                         bitmap = bitmapSet.getBitmap(45);
                         break;
+                    case '#':
+                        bitmap = bitmapSet.getBitmap(45);
+                        break;
                     default:
                         bitmap = bitmapSet.getBitmap(23);
                         break;
@@ -112,75 +114,10 @@ public class HistoryScene {
     }
 
     public char[][] updateMap() {
-        int platforms = 0;
-        int platformsLong1 = 0;
-        int platformsLong2 = 0;
-        int platformsLong3 = 0;
-        int groundCounter = 0;
-        int rng = 0;
-        int creatingPlatformLong = 0;
-        int lastPlatformAltitude = 15;
-        int checkRng = 0;
-        int lastPlatformX = 20;
-        platformsDistance++;
-        // buscamos info
         for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < 29; j++) {
-                // contamos las plataformas
-                if (scene2[i][j] == ('-'/* plataforma */) && scene2[i][j + 1] == ('.' /* cielo */)) {
-                    platforms++;
-                    lastPlatformX = j;
-                }
-                // contamos el suelo
-                if (i == 15 && scene2[15][j] == '-') {
-                    groundCounter++;
-                }
-                if ( scene2[i][29] == ('-'/* plataforma */)){
-                    lastPlatformAltitude = i;
-                }
-            }
+            scene[i] = scene[i].substring(1);
         }
-        // avanzamos el scene una posicion
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < 29; j++) {
-                scene2[i][j] = scene2[i][j + 1];
-            }
-        }
-        // pintamos la ultima linea
-        for (int i = 0; i < 16; i++) {
-//            if (i == 15 && groundCounter < 3) {
-//                scene[i][29] = '-';
-//                creatingPlatform = true;
-//            }else {
-            //pintar plataforma nueva
-            if (platforms < 6 && i == 15 && lastPlatformX<25 && !creatingPlatform) {
-                do {
-                    rng = (int) (Math.random() * 15 );
-                    checkRng = rng-lastPlatformAltitude;
-                }while(rng<6 || checkRng>2 || checkRng<(-2) );
-                scene2[rng][29] = '-';
-                platformsDistance = 0;
-                creatingPlatform = true;
-            }else {
-                // pintar cielo
-                scene2[i][29] = '.';
-            }
-            // random de la longitud de la plataforma
-            if (scene2[i][28] == '-') {
-                for (int j = 28; j > 24; j--) {
-                    if (scene2[i][j] == '-')
-                        creatingPlatformLong++;
-                }
-                rng = (int) (Math.random() * 10 + creatingPlatformLong);
 
-                if (rng < 11)
-                    scene2[i][29] = '-';
-                else
-                    creatingPlatform = false;
-            }
-
-        }
-//        }
         return scene2;
 
     }
