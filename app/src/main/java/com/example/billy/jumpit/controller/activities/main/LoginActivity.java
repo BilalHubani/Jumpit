192646ccdd4a429c318f9ad2d243aa6e0954ad8f
Yuna114 <aheila.ros.cheme@gmail.com>
2017-06-04 19:15:01 +0200

    #8 Implementando login/registro

---
+++ app/src/main/java/com/example/billy/jumpit/controller/activities/main/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.billy.jumpit.controller.activities.main;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.billy.jumpit.R;
+import com.example.billy.jumpit.controller.managers.LoginCallback;
+import com.example.billy.jumpit.controller.managers.UserLoginManager;
+import com.example.billy.jumpit.model.UserToken;
+
+
+/**
+ * A login screen that offers login via username/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+}
+
