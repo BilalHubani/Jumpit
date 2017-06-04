3d7664272eda96a7f0f7f194913115add6a9e8e3
Kimsb1994 <kimsb1994@gmail.com>
2017-06-05 00:41:35 +0200

    #3 acabando cositas

--- app/src/main/java/com/example/billy/jumpit/controller/managers/SkinManager.java
+++ app/src/main/java/com/example/billy/jumpit/controller/managers/SkinManager.java
@@ -67,7 +67,6 @@ public class SkinManager {
             @Override
             public void onFailure(Call<List<Skin>> call, Throwable t) {
                 Log.e("SkinManager->", "getAllSkins()->ERROR: " + t);
-
                skinCallback.onFailure(t);
             }
         });
